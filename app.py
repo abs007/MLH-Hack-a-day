79b27b5a0442790a6288521529d9fd3c95665910
SaraBedd <78218394+SaraBedd@users.noreply.github.com>
2022-03-05 20:54:25 -0500

    login, signup and predict

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from predictor import scaler
 model = pickle.load(open('model.pkl','rb'))
 
 
-def predict():
-
-    int_features=[int(x) for x in request.form.values()]
+def predict(*args):
+    #int_features= args
+    int_features=[int(x) for x in args]
 
     user_data = np.array(int_features).reshape((1,-1))
     
     user_data_scaled = scaler.transform(user_data)
 
-    prediction = model.predict_(user_data_scaled)
+    prediction = model.predict(user_data_scaled)
     print (prediction)
+
